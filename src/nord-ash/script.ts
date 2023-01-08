e2c51416915ea3e0cd4aaf81841802c00e50ce00
demogoran <demogoran@gmail.com>
2023-01-08 21:06:39 +0200

    Add sucrace

--- src/nord-ash/script.ts
+++ src/nord-ash/script.ts
@@ -2,11 +2,36 @@
 import "frida-il2cpp-bridge";
 import { addListener, updateFunctions } from "../utils/helpers";
 
+const startListeners = () => {
+  /* addListener("Assembly-CSharp", "Stats", ["SimpleAddStats"]);
+  addListener("Assembly-CSharp", "Stats", ["SimpleMultiplyStats"]);
+  addListener("Assembly-CSharp", "Stats", ["CopyStats"]); */
+
+  const callback = function ({ method }, itemXP) {
+    const obj = itemXP as Il2Cpp.Object;
+    console.log(obj);
+    //console.log(obj.field("_poolAmount"), obj.field("_poolAmount")?.value);
+    //obj.field("_poolAmount").value = 100;
+    return this.method(method).invoke(itemXP);
+  };
+  updateFunctions(true, "ExperienceManager", "AddXP", callback);
+
+  /* updateFunctions(
+    true,
+    "EliminateSpecificEntitiesObjective",
+    ".ctor",
+    function ({ method }, id, total) {
+      console.log("Elim", id, total);
+      return this.method(method).invoke(id, 1);
+    }
+  ); */
+};
+
 async function main() {
   try {
     await Il2Cpp.initialize();
     Il2Cpp.perform(() => {
-      Il2Cpp.dump("dump.cs");
+      startListeners();
     });
     console.log("Done");
   } catch (ex) {
